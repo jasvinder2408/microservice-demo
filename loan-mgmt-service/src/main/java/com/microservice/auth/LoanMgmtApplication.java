b40605f3c11bcf2a598264a66cc4365ff5364225
jasvinder2408 <jasvinderlucky.singh@gmail.com>
2024-11-17 21:54:49 +0530

    initialc commit

---
+++ loan-mgmt-service/src/main/java/com/microservice/auth/LoanMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMgmtApplication.class, args);
+	}
+
+}
